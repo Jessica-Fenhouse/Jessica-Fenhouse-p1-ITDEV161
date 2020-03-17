a3d15980117871bf159c39035818638b48ffe1ba
Jessica Fenhouse <Fenhousj@gmatc.matc.edu>
2020-03-17 11:08:45 -0500

    finished project 1

---
+++ routes/main.js
@@ -0,0 +1,24 @@
+const vertex = require('vertex360')({ site_id: process.env.TURBO_APP_ID })
+const router = vertex.router()
+const ProjectController = require('../controllers/ProjectController')
+
+router.get('/', (req, res) => {
+    const data = req.context
+
+    const projectCtr = new ProjectController()
+    projectCtr.get()
+    .then(projects => {
+        data['projects'] = projects
+        res.render('landing', data)
+    })
+    .catch(err => {
+        res.send('Oops! '+err.message)
+    })
+
+    
+    
+    
+})
+
+
+module.exports = router
